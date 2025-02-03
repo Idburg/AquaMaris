5c48b5ff3d7ec952b87194afbf7702750a5fbacf
Idlir <idlirbregu02@gmail.com>
2025-02-03 14:00:54 +0100

    Añadido intent en Login.java para pasar al MainActivity

--- app/src/main/java/com/example/aquamaris/Login.java
+++ app/src/main/java/com/example/aquamaris/Login.java
@@ -1,9 +1,11 @@
 package com.example.aquamaris;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,6 +17,13 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        AppCompatButton logButton = findViewById(R.id.login_button);
+        logButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
