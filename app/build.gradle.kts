15e98f5c2eb408e0a673e25462508260428c4b8a
Idlir <idlirbregu02@gmail.com>
2025-02-20 10:51:33 +0100

    - VERSION 0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.proyecto.aquamaris"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.6"
+        versionCode = 5
+        versionName = "0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
