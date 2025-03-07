05e74810c8bfa64a266246811ff508eb03918879
MaRiOo0o <masansedi@gmail.com>
2025-03-07 10:33:05 +0100

    Aviso legal terminado

--- app/src/main/java/com/proyecto/aquamaris/AvisoLegal.java
+++ app/src/main/java/com/proyecto/aquamaris/AvisoLegal.java
@@ -1,12 +1,17 @@
 package com.proyecto.aquamaris;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.proyecto.aquamaris.Fragmentos.Prueba3;
 
 public class AvisoLegal extends AppCompatActivity {
 
@@ -15,10 +20,27 @@ public class AvisoLegal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_aviso_legal);
+
+        Toolbar toolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);  // Establece el Toolbar como ActionBar
+
+        // Configura la ActionBar y habilita la flecha "volver"
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);  // Habilita la flecha de "volver"
+            getSupportActionBar().setDisplayShowHomeEnabled(true);  // Asegúrate de que el ícono de la home se vea
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
