effd1e5120418a378be6d01b4cc12db3116717b4
Idlir <idlirbregu02@gmail.com>
2025-03-07 13:57:55 +0100

    - Version 1.3 PUBLISHED

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.proyecto.aquamaris"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.2"
+        versionCode = 13
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
